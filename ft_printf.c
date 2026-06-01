aeaacf829d5410f9352dafef20ca4efe25a99352
light-11 <violet121311@gmail.com>
2026-06-01 17:09:07 +0900

    %pでnullが来た場合に0x0が出力される不具合(?)を修正

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ayanaga <ayanaga@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 19:16:07 by ayanaga           #+#    #+#             */
-/*   Updated: 2026/05/20 18:32:23 by ayanaga          ###   ########.fr       */
+/*   Updated: 2026/06/01 16:43:18 by ayanaga          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,31 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-static int	check(char c, va_list args)
+static int	check_p(unsigned long n)
 {
-	int	count;
+	int		count;
 
 	count = 0;
+	if (!n)
+	{
+		count = ft_putstr("(nil)");
+	}
+	else
+	{
+		count += ft_putstr("0x");
+		count += ft_puthex(n, 0);
+	}
+	return (count);
+}
+
+static int	check(char c, va_list args)
+{
 	if (c == 'c')
 		return (ft_putchar(va_arg(args, int)));
 	if (c == 's')
 		return (ft_putstr(va_arg(args, char *)));
 	if (c == 'p')
-	{
-		count += ft_putstr("0x");
-		count += ft_puthex((unsigned long)va_arg(args, void *), 0);
-		return (count);
-	}
+		return (check_p((unsigned long)va_arg(args, void *)));
 	if (c == 'd' || c == 'i')
 		return (ft_putnbr(va_arg(args, int)));
 	if (c == 'u')
@@ -75,16 +85,25 @@ int	ft_printf(const char *format, ...)
 
 //int	main(void)
 //{
-//	int	a;
-//	int	r1;
-//	int	r2;
+//	int		a;
+//	char	*b;
+//	int		r1;
+//	int		r2;
+//	int		b1;
+//	int		b2;
 
 //	a = 5;
+//	b = NULL;
 //	r1 = ft_printf("char=%c str=%s int=%d hex=%x HEX=%X percent=%% ptr=%p\n",
 //			'A', "hello", -123, 255, 255, &a);
 //	r2 = printf("char=%c str=%s int=%d hex=%x HEX=%X percent=%% ptr=%p\n", 'A',
 //			"hello", -123, 255, 255, &a);
 //	ft_printf("ft return: %d\n", r1);
-//	printf("pf return: %d\n", r2);
+//	printf("return: %d\n", r2);
+
+//	b1 = ft_printf("%p\n", b);
+//	b2 =printf("%p\n", b);
+//	ft_printf("ft return: %d\n", b1);
+//	printf("return: %d\n", b2);
 //	return (0);
 //}
